bf951c94955c7beead5e17a3d9720bea18957f6f
vishalrana-zt <vishal.rana@zentrades.pro>
2026-01-13 16:11:08 +0530

    update

--- Sources/ViewControllers/FPFormViewController.swift
+++ Sources/ViewControllers/FPFormViewController.swift
@@ -2267,8 +2267,8 @@ extension FPFormViewController: UIPickerViewDelegate {
             for row in 0..<rows {
                 let indexPath = IndexPath(row: row, section: section)
                 guard let sectionItem = FPFormDataHolder.shared.getRowForSection(self.section, at: indexPath.row) else{ continue }
-                if sectionItem.getUIType() == .TABLE || field.getUIType() == .TABLE_RESTRICTED{
-                    idexs.append(idexs)
+                if sectionItem.getUIType() == .TABLE || sectionItem.getUIType() == .TABLE_RESTRICTED{
+                    idexs.append(indexPath)
                 }
             }
             if !idexs.isEmpty{
