d57d8e71fae32d94de51695b086fa0a9413be4f5
vishalrana-zt <vishal.rana@zentrades.pro>
2026-02-11 19:15:51 +0530

    update asset id sort position in offline case

--- Sources/ViewControllers/FPFormViewController.swift
+++ Sources/ViewControllers/FPFormViewController.swift
@@ -2841,8 +2841,8 @@ extension FPUtility{
         //edge case where we adding same type of asset via scanner in same fpform which causes incorrect payload
         if !sectionTemplateId.isEmpty,  let localSectionId = linkingData.sectionLocalId {
             if let secIndex = FPFormDataHolder.shared.customForm?.sections?.firstIndex(where: {$0.templateId == sectionTemplateId && $0.sqliteId == localSectionId}){
-                if let sectionToUpdate = FPFormDataHolder.shared.customForm?.sections?[safe:secIndex], let assetId = linkingData.assetId{
-                    let hiddenAssetField = FPFormDataHolder.shared.getHiddenAssetField(assetId: assetId, sortPostion: "\(sectionToUpdate.fields.first?.sortPosition ?? "00")0")
+                if let sectionToUpdate = FPFormDataHolder.shared.customForm?.sections?[safe:secIndex], let assetId = linkingData.assetId, let sortPosition = sectionToUpdate.fields.last?.sortPosition{
+                    let hiddenAssetField = FPFormDataHolder.shared.getHiddenAssetField(assetId: assetId, sortPostion: sortPosition)
                     FPFormDataHolder.shared.customForm?.sections?[secIndex].fields.append(hiddenAssetField)
                     return
                 }
@@ -2852,8 +2852,8 @@ extension FPUtility{
         if let secIndex = FPFormDataHolder.shared.customForm?.sections?.firstIndex(where: { section in
             return sectionTemplateId.isEmpty ? section.sqliteId == linkingData.sectionLocalId :  (section.sqliteId == linkingData.sectionLocalId || section.templateId == linkingData.sectionTemplateId)
         }){
-            if let sectionToUpdate = FPFormDataHolder.shared.customForm?.sections?[safe:secIndex], let assetId = linkingData.assetId{
-                let hiddenAssetField = FPFormDataHolder.shared.getHiddenAssetField(assetId: assetId, sortPostion: "\(sectionToUpdate.fields.first?.sortPosition ?? "00")0")
+            if let sectionToUpdate = FPFormDataHolder.shared.customForm?.sections?[safe:secIndex], let assetId = linkingData.assetId, let sortPosition = sectionToUpdate.fields.last?.sortPosition{
+                let hiddenAssetField = FPFormDataHolder.shared.getHiddenAssetField(assetId: assetId, sortPostion: sortPosition)
                 FPFormDataHolder.shared.customForm?.sections?[secIndex].fields.append(hiddenAssetField)
             }
         }
