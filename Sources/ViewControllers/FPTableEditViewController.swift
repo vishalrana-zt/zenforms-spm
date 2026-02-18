b7ffe980b5dc6e0aba4ca341985e1f5fee63ef3d
vishalrana-zt <vishal.rana@zentrades.pro>
2026-02-18 20:30:50 +0530

    limit result to max 2 decimal points

--- Sources/ViewControllers/FPTableEditViewController.swift
+++ Sources/ViewControllers/FPTableEditViewController.swift
@@ -1376,10 +1376,10 @@ extension UIImageView {
 
 extension Double {
 
-    func formattedMax4Decimal() -> String {
+    func formattedMax2Decimal() -> String {
         let formatter = NumberFormatter()
         formatter.minimumFractionDigits = 0
-        formatter.maximumFractionDigits = 4
+        formatter.maximumFractionDigits = 2
         formatter.numberStyle = .decimal
         
         return formatter.string(from: NSNumber(value: self)) ?? "\(self)"
