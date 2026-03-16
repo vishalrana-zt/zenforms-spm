8218febaaba727561cb75675159815385552e0b9
vishalrana-zt <vishal.rana@zentrades.pro>
2026-03-16 17:19:41 +0530

    Update ZenForms.swift

--- Sources/ZenForms.swift
+++ Sources/ZenForms.swift
@@ -146,12 +146,13 @@ public final class ZenForms {
     }
     
     @MainActor  private class func setupKeyboard(){
+        IQKeyboardToolbarManager.shared.toolbarConfiguration.barTintColor = .systemBackground
         IQKeyboardManager.shared.keyboardDistance = 20
-        IQKeyboardManager.shared.toolbarConfiguration.previousNextDisplayMode = .alwaysShow
+        IQKeyboardToolbarManager.shared.toolbarConfiguration.previousNextDisplayMode = .alwaysShow
         IQKeyboardManager.shared.isEnabled = false
-        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardToolbarManager.shared.isEnabled = false
         IQKeyboardManager.shared.resignOnTouchOutside = true
-        IQKeyboardManager.shared.deepResponderAllowedContainerClasses  = [UITableView.self, UICollectionView.self, UIStackView.self, UIView.self, UIScrollView.self]
+        IQKeyboardToolbarManager.shared.deepResponderAllowedContainerClasses  = [UITableView.self, UICollectionView.self, UIStackView.self, UIView.self, UIScrollView.self]
     }
    
     public func syncZenForm(form:FPForms, syncDelegate: ZenFormsSyncAssetLinkingDelegate?, isAssetEnabled:Bool , completion: @escaping (_ success: Bool) -> ()){
