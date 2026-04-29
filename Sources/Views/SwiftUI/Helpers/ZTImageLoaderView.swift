8ae6582669d4e6aa646fce368bdcff5428c724ef
vishalrana-zt <vishal.rana@zentrades.pro>
2026-04-29 17:27:11 +0530

    minor impv

--- Sources/Views/SwiftUI/Helpers/ZTImageLoaderView.swift
+++ Sources/Views/SwiftUI/Helpers/ZTImageLoaderView.swift
@@ -13,17 +13,33 @@ struct ZTImageLoaderView: View {
     var urlString: String = ""
     var resizingMode: SwiftUI.ContentMode = .fit
     
+    @State private var loadingFailed = false
+    
     var body: some View {
         Rectangle()
             .opacity(0.001)
             .overlay {
-                KFImage(URL(string: urlString))
-                    .placeholder {
-                        ProgressView()
-                    }
-                    .resizable()
-                    .aspectRatio(contentMode: resizingMode)
-                    .allowsHitTesting(false)
+                if loadingFailed {
+                    Image("noimage")
+                        .resizable()
+                        .aspectRatio(contentMode: resizingMode)
+                } else {
+                    KFImage(URL(string: urlString))
+                        .placeholder {
+                            ProgressView()
+                        }
+                        .onFailure { error in
+                            loadingFailed = true
+                        }
+                        .retry(maxCount: 1, interval: .seconds(2))
+                        .onSuccess { result in
+                            loadingFailed = false
+                        }
+                        .fade(duration: 0.25)
+                        .resizable()
+                        .aspectRatio(contentMode: resizingMode)
+                        .allowsHitTesting(false)
+                }
             }
             .clipped()
     }
